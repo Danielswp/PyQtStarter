51a83d3eca52e4f9ce1eb4f1161cabc4674b67b2
Daniel_shi <enhot520@126.com>
2022-11-04 16:11:15 +0800

    dictionary test

---
+++ pyDictionaryTest.py
@@ -0,0 +1,20 @@
+#1
+print('\n#1')
+zdict={}
+zdict['w1']='hello'
+zdict['w2']='yhqj'
+print('zdict',zdict)
+
+#2
+print('\n#2')
+vdict={
+	'student1':'jack',
+	'student2':'tom',
+	'student3':'lily',
+}
+print('vdict',vdict)
+
+#3
+print('\n#3')
+s2=zdict['w1'];print('s2',s2)
+s3=vdict['student2'];print('s3',s3)
